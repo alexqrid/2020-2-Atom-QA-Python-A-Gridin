79b3f9934a128ea2e3afbd375066e95aa29723d3
Alexander Gridin <alexqrid@gmail.com>
2020-12-07 00:42:59 +0400

    HW6 done

---
+++ QA-Python-homework-6/settings.py
@@ -0,0 +1,9 @@
+from urllib.parse import urljoin
+
+TIMEOUT = 1.5
+
+APP_HOST, APP_PORT = '127.0.0.1', 8000
+APP_URL = f'http://{APP_HOST}:{APP_PORT}'
+
+MOCK_HOST, MOCK_PORT = '127.0.0.1', 8003
+MOCK_URL = f'http://{MOCK_HOST}:{MOCK_PORT}'
